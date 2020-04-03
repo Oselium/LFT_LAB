654cfe1a39668754f75b32bac054e28b31e33a17
Oselium <34212471+Oselium@users.noreply.github.com>
2020-04-03 18:38:21 +0200

    Add files via upload

---
+++ NumberTok.java
@@ -0,0 +1,5 @@
+public class NumberTok extends Token {
+public int lexeme;
+public NumberTok(int s) { super(256); lexeme=s; }
+public String toString() { return "<" + tag + ", " + lexeme + ">"; }
+}
