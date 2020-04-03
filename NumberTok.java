1210585b77aa01f3d68bba0884d250b81bf10b65
Oselium <34212471+Oselium@users.noreply.github.com>
2020-04-03 18:39:44 +0200

    Delete NumberTok.java

--- NumberTok.java
+++
@@ -1,5 +0,0 @@
-public class NumberTok extends Token {
-public int lexeme;
-public NumberTok(int s) { super(256); lexeme=s; }
-public String toString() { return "<" + tag + ", " + lexeme + ">"; }
-}
